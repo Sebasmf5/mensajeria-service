a7514161f6802b93c1f78692f0c7d7bf2acdee85
Sebastian <139397006+Sebasmf5@users.noreply.github.com>
2025-03-29 23:08:14 -0500

    feat: Creacion del repositorio para la entidad mensajeria

--- src/main/java/co/edu/uceva/mensajeriaservice/model/entities/Mensajeria.java
+++ src/main/java/co/edu/uceva/mensajeriaservice/model/entities/Mensajeria.java
@@ -1,4 +1,22 @@
 package co.edu.uceva.mensajeriaservice.model.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.GenerationType;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
 public class Mensajeria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String correo;
+    private String correoDestinatario;
+    private String titulo;
+    private String cuerpoMensaje;
+
 }
--- src/main/java/co/edu/uceva/mensajeriaservice/model/repositories/IMensajeriaRepository.java
+++ src/main/java/co/edu/uceva/mensajeriaservice/model/repositories/IMensajeriaRepository.java
@@ -1,4 +1,10 @@
 package co.edu.uceva.mensajeriaservice.model.repositories;
 
-public interface IMensajeriaRepository {
+import co.edu.uceva.mensajeriaservice.model.entities.Mensajeria;
+import org.springframework.data.repository.CrudRepository;
+/**
+ * Interface que hereda de CrudRepository para realizar las
+ * operaciones de CRUD sobre la entidad Producto
+ */
+public interface IMensajeriaRepository extends CrudRepository<Mensajeria, Long> {
 }
